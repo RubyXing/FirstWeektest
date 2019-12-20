af325e6fe3874e140600853a3b5ebfeb73140ca6
RubyXing <593680319@qq.com>
2019-12-20 17:45:52 +0800

    XQL Class PC commit: Homework

--- Day5Code/src/ClassCode5.java
+++ Day5Code/src/ClassCode5.java
@@ -8,18 +8,18 @@ public class ClassCode5 {
 //练习:
 //1、定义两个int型数组firstBuf和secondBuf ,将其连接为一个大数组buf.
 //2、对buf数组进行升序排序，要求不使用自带的sort方法。
-        int[] firstBuf,secondBuf;
-        firstBuf=new int[]{1,4,7,9};
-        secondBuf=new int[]{2,9,6,3};
-        int[] buf=new int[firstBuf.length+secondBuf.length];
+        int[] firstBuf, secondBuf;
+        firstBuf = new int[]{1, 4, 7, 9};
+        secondBuf = new int[]{2, 9, 6, 3};
+        int[] buf = new int[firstBuf.length + secondBuf.length];
         for (int i = 0; i < firstBuf.length; i++) {
-            buf[i]=firstBuf[i];
+            buf[i] = firstBuf[i];
         }
         for (int i = firstBuf.length; i < buf.length; i++) {
-            buf[i]=secondBuf[i-firstBuf.length];
+            buf[i] = secondBuf[i - firstBuf.length];
         }
-        for (int j = 0; j < buf.length; j++) {
-            for (int i = 0; i < buf.length - 1; i++) {
+        for (int j = buf.length; j >= 0; j--) {
+            for (int i = 0; i < j - 1; i++) {
                 if (buf[i] > buf[i + 1]) {
                     buf[i + 1] = buf[i] + buf[i + 1];
                     buf[i] = buf[i + 1] - buf[i];
